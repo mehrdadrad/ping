0857105ce1fc5f704008221fb08613f2f3683da1
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-05-07 16:59:53 -0700

    add more test

--- ping_test.go
+++ ping_test.go
@@ -2,6 +2,7 @@ package ping
 
 import (
 	"math/rand"
+	"net"
 	"testing"
 	"time"
 )
@@ -22,9 +23,41 @@ var p = Ping{
 	source:     "",
 }
 
+func TestNew(t *testing.T) {
+	_, err := New("127.0.0.1")
+	if err != nil {
+		t.Error("New failed:", err)
+	}
+}
+
 func TestGetTimeStamp(t *testing.T) {
 	ts1 := time.Now().UnixNano()
 	pl := p.payload(ts1)
 	ts2 := getTimeStamp(pl)
-	t.Log(ts1, ts2)
+	if ts1 != ts2 {
+		t.Error("timestamp failed")
+	}
+}
+
+func TestGetIPAddr(t *testing.T) {
+	i := net.UDPAddr{
+		IP:   net.ParseIP("192.168.10.1"),
+		Port: 1000,
+	}
+
+	o := p.getIPAddr(&i)
+
+	if o != "192.168.10.1" {
+		t.Error("getIPAddr UDPAdd failed")
+	}
+
+	ii := net.IPAddr{
+		IP: net.ParseIP("192.168.10.1"),
+	}
+
+	o = p.getIPAddr(&ii)
+
+	if o != "192.168.10.1" {
+		t.Error("getIPAddr UDPAdd failed")
+	}
 }
